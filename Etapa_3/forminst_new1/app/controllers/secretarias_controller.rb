520aa8fe3c5890c356e252d8a137a74256609ef6
carlosjj24 <iglesiasmonsalve.carlosjulio@gmail.com>
2017-02-17 00:09:45 -0400

    Continuacion de la secretaria, falta que al agregar el usuario se agregue el estatus de la adecuacion

--- Etapa_3/forminst_new1/app/controllers/secretarias_controller.rb
+++ Etapa_3/forminst_new1/app/controllers/secretarias_controller.rb
@@ -25,22 +25,24 @@ class SecretariasController < ApplicationController
 			@nombre = session[:nombre_usuario]
 			@personas = Persona.all
 			session[:personas] = @personas.as_json(only: [:usuario_id, :nombres, :apellidos])
-			@escuelas = [['Seleccione una escuela',0], ['Escuela de Biología',1], ['Escuela de Computación',2], ['Escuela de Física',3], ['Escuela de Geoqímica',4], ['Instituto Biología Experimental',5], ['Instituto de Ciencia y Tecnología de Alimentos',6], ['Instituto de Ciencias de la Tierra',7], ['Instituto de Zoología y Ecología Tropical',8], ['Escuela de Matemática',9], ['Escuela de Química',10], ['Consejo de Facultad',11], ['Desconocida',12]] 
-			uentidad = Usuarioentidad.where(escuela_id: 2, entidad_id: 18)
+			cpSecretariaID = session[:usuario_id]
+			cpSecretaria = Usuarioentidad.where(usuario_id: cpSecretariaID).take
+			cpSecretariaEscuela = cpSecretaria.escuela_id
+
+			uentidad = Usuarioentidad.where(escuela_id: cpSecretariaEscuela, entidad_id: 18)
 			cpcontador = 0
 			cpaux = ['var1', 0]
 			@cptutores = []
+			@cptutores[cpcontador] = Array.new(2) { |i|  }
+			@cptutores[cpcontador][0] =  "Seleccione un tutor"
+			@cptutores[cpcontador][1] = 0
+			cpcontador = cpcontador + 1
 
 			uentidad.each do |usuarioentidad|
-				puts "holaaaaaaaaaaaaaaaaaaaaaaaaaa"
 				cppersona = Persona.find_by usuario_id: usuarioentidad.usuario_id
 				@cptutores[cpcontador] = Array.new(2) { |i|  }
-				puts cppersona.nombres
 				@cptutores[cpcontador][0] =  cppersona.nombres
-				puts @cptutores[cpcontador][0]
 				@cptutores[cpcontador][1] = usuarioentidad.usuario_id
-				puts @cptutores[cpcontador][1]
-				puts @cptutores
 				cpcontador = cpcontador + 1
 			end
 
@@ -52,6 +54,9 @@ class SecretariasController < ApplicationController
 
 	def guarda_instructor
 		if session[:usuario_id]	
+			puts "holaaaaaaaaaaaaaaaaaaaaaaaaaa"
+			puts params[:JRTutores]
+
 			cpusuario = Usuario.new
 			cpusuario.user = params[:correo]
 			cpusuario.password = params[:CI]
@@ -59,8 +64,7 @@ class SecretariasController < ApplicationController
 			cpusuario.activo = 1
 			cpusuario.tipo = "Docente"
 			cpusuario.email = params[:correo]
-
-			puts params[:tutor]
+			cpusuario.save
 
 			cppersona = Persona.new
 			cppersona.usuario_id = cpusuario.id
@@ -74,12 +78,37 @@ class SecretariasController < ApplicationController
 			cppersona.grado_instruccion = params[:GradoI]
 			cppersona.area = params[:Area]
 			cppersona.subarea = params[:Subarea]
+			cppersona.save
 
 			cpuentidad = Usuarioentidad.new
 			cpuentidad.usuario_id = cpusuario.id
 			cpuentidad.entidad_id = 19
 			cpuentidad.id = cpusuario.id
 			cpuentidad.escuela_id = params[:escuela]
+			cpuentidad.save
+
+			cpplanformacion = Planformacion.new
+			cpplanformacion.fecha_inicio = params[:FechaConcurso]
+			cpplanformacion.activo = 1
+			cpplanformacion.instructor_id = cpusuario.id
+			cpplanformacion.tutor_id = params[:JRTutores]
+			cpplanformacion.adscripcion_docencia = params[:UAD]
+			cpplanformacion.adscripcion_investigacion = params[:UAI]
+			cpplanformacion.save
+
+			cpAdecuacion = Adecuacion.new
+			cpAdecuacion.planformacion_id = cpplanformacion.id
+			cpAdecuacion.tutor_id = cpplanformacion.tutor_id
+			cpAdecuacion.fecha_creacion = cpplanformacion.fecha_inicio
+			cpAdecuacion.save
+
+			cpEstatus_Adecuacion = Estatusadecuacion.new
+			cpEstatus_Adecuacion.adecuacion_id = cpAdecuacion.id
+			cpEstatus_Adecuacion.fecha 
+
+
+
+
 
 			redirect_to controller:"secretarias", action: "index"
 		else
