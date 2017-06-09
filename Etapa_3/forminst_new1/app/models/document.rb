49db80abbd461713f5d1d5fa5ae06c63e3b9194f
pedroaro <pedroaro123@gmail.com>
2017-06-09 14:59:25 -0400

    Arreglado tamaño de soporte de 64KB a 1MB, fixed minor errors

--- Etapa_3/forminst_new1/app/models/document.rb
+++ Etapa_3/forminst_new1/app/models/document.rb
@@ -6,9 +6,9 @@ class Document < ActiveRecord::Base
     @file = params.delete(:file)
     super
     if @file
-      self.filename = sanitize_filename(@file.original_filename)
-      self.content_type = @file.content_type
-      self.file_contents = @file.read
+        self.filename = sanitize_filename(@file.original_filename)
+        self.content_type = @file.content_type
+        self.file_contents = @file.read
     end
   end
 
@@ -22,8 +22,8 @@ class Document < ActiveRecord::Base
 
     NUM_BYTES_IN_MEGABYTE = 1048576
     def file_size_under_one_mb
-      if (@file.size.to_f / NUM_BYTES_IN_MEGABYTE) > 10
-        errors.add(:file, 'File size cannot be over one megabyte.')
+      if (@file.size.to_f / NUM_BYTES_IN_MEGABYTE) > 1
+        errors.add(:file, 'El tamaño del archivo no puede ser mayor a 1 MB.')
       end
     end
 
